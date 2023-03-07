3518bbb46c063de1495df4c2be5d657c4ec8fdec
adimerpaul <adimer101@gmail.com>
2023-03-07 19:56:07 -0400

    comenzado

---
+++ back/database/migrations/2014_10_11_000000_create_shops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('avatar')->nullable()->default('logo.png');
+            $table->string('type')->nullable();
+            $table->string('name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('document')->nullable();
+            $table->string('status')->default('inactive');
+            $table->foreignId('user_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shops');
+    }
+};
