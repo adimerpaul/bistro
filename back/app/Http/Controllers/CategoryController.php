fe101a448a5c3fa2f6f45b9f57ad792e13066217
adimerpaul <adimer101@gmail.com>
2023-05-18 21:33:46 -0400

    upload

--- back/app/Http/Controllers/CategoryController.php
+++ back/app/Http/Controllers/CategoryController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\category;
+use App\Models\Category;
 use App\Http\Requests\StorecategoryRequest;
 use App\Http\Requests\UpdatecategoryRequest;
 
 class CategoryController extends Controller{
-    public function index(){return category::all();}
-    public function show($shop_id){return category::where('shop_id', $shop_id)->get();}
-    public function store(StorecategoryRequest $request){return category::create($request->all());}
-    public function update(UpdatecategoryRequest $request, category $category){$category->update($request->all());return $category;}
-    public function destroy(category $category){$category->delete();return $category;}
+    public function index(){return Category::all();}
+    public function show($shop_id){return Category::where('shop_id', $shop_id)->get();}
+    public function store(StorecategoryRequest $request){return Category::create($request->all());}
+    public function update(UpdatecategoryRequest $request, Category $category){$category->update($request->all());return $category;}
+    public function destroy(Category $category){$category->delete();return $category;}
 }
--- back/app/Models/Documentsector.php
+++ back/app/Models/Documentsector.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DocumentSector extends Model
+class Documentsector extends Model
 {
     use HasFactory;
     protected $table='documentsectors';
