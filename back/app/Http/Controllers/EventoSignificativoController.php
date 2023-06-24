2a77909e93ce7c93ed1ccb412d958911d2f16ef4
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2023-06-24 19:01:59 -0400

    subir

--- back/app/Http/Controllers/EventoSignificativoController.php
+++ back/app/Http/Controllers/EventoSignificativoController.php
@@ -139,7 +139,7 @@ class EventoSignificativoController extends Controller
         $firmar = new Firmar();
         $archivo=$firmar->getFileGzip("archivos/archive.tar.gz");
         $hashArchivo=hash('sha256', $archivo);
-//        unlink("archivos/archive.tar.gz");
+        unlink("archivos/archive.tar.gz");
 
 
         $client = new \SoapClient(env('URL_SIAT')."ServicioFacturacionCompraVenta?WSDL",  [
