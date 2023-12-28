0ad80796fc789e6344992f914651286e4cce9a89
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2023-12-27 22:24:56 -0400

    imp

--- front/src/pages/Sales.vue
+++ front/src/pages/Sales.vue
@@ -734,7 +734,7 @@ export default {
       // eslint-disable-next-line no-multi-str
       cadena += "</tbody></table><hr>\
       <table style='font-size: 8px;'>\
-      <tr><td class='titder'>TOTAL Bs</td><td class='conte2'>" + parseFloat(total).toFixed(2) + '</td></tr> </table><br> <div>Son ' + a + ' ' + (parseFloat(total).toFixed(2) - Math.floor(parseFloat(total).toFixed(2))) * 100 + ' /100 Bolivianos</div><div>Mesa: <b style="font-size:24px; text-align: center">' + order.mesa + '</b></div>'
+      <tr><td class='titder'>TOTAL Bs</td><td class='conte2'>" + parseFloat(total).toFixed(2) + '</td></tr> </table><br> <div>Son ' + a + ' ' + (parseFloat(total).toFixed(2) - Math.floor(parseFloat(total).toFixed(2))) * 100 + ' /100 Bolivianos</div><div style="text-align: center">Mesa: <b style="font-size:24px; ">' + order.mesa + '</b></div>'
       document.getElementById('myelement').innerHTML = cadena
       const d = new Printd()
       d.print(document.getElementById('myelement'))
@@ -800,7 +800,7 @@ export default {
       // eslint-disable-next-line no-multi-str
       cadena += "</tbody></table><hr>\
       <table style='font-size: 8px;'>\
-      <tr><td class='titder'>TOTAL Bs</td><td class='conte2'>" + parseFloat(total).toFixed(2) + '</td></tr> </table><br> <div>Son ' + a + ' ' + (parseFloat(total).toFixed(2) - Math.floor(parseFloat(total).toFixed(2))) * 100 + ' /100 Bolivianos</div><div>Mesa: <b style="font-size:24px; text-align: center">' + order.mesa + '</b></div><div><b>AGREGADO AL PEDIDO</b></div>'
+      <tr><td class='titder'>TOTAL Bs</td><td class='conte2'>" + parseFloat(total).toFixed(2) + '</td></tr> </table><br> <div>Son ' + a + ' ' + (parseFloat(total).toFixed(2) - Math.floor(parseFloat(total).toFixed(2))) * 100 + ' /100 Bolivianos</div><div style="text-align: center">Mesa: <b style="font-size:24px; ">' + order.mesa + '</b></div><div style="text-align: center"><b>AGREGADO AL PEDIDO</b></div>'
       document.getElementById('myelement').innerHTML = cadena
       const d = new Printd()
       d.print(document.getElementById('myelement'))
